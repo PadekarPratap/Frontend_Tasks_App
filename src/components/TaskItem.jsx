10906ef2c09f35a69863e9f2a7819300dc396d27
Pratap Padekar <unitedpratap7@gmail.com>
2023-06-05 20:23:34 +0530

    minor fix

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -31,7 +31,7 @@ const TaskItem = ({ task }) => {
   return (
     <div className={`flex items-center justify-between mt-5 ${isCompleted && 'opacity-50'}`}>
       <div>
-        <p className={`text-lg font-bold capitalize ${isCompleted && 'line-through'}`}>{title}</p>
+        <p className={`text-lg font-bold ${isCompleted && 'line-through'}`}>{title}</p>
         <p className="text-sm ">{description}</p>
       </div>
       <div className="flex items-center gap-4">
